18c1f76aae4e8e2bf6ac914bede4d35346ca2243
ramrami-B <21801034@handong.ac.kr>
2023-10-31 15:44:05 +0900

    Fix: 배포 오류 수정(소문자로 시작하는 파일 이름 대문자로 변경, 중복 파일 삭제)

---
+++ src/app/components/modal/Modal.tsx
@@ -0,0 +1,100 @@
+import { AiOutlineClose } from "react-icons/ai";
+import { useEffect, useState } from "react";
+import PebbleImage from "../PebbleImage";
+import { PEBBLE_STATUS } from "../../service/constants";
+import GreenButton from "../GreenButton";
+import { modalStyle } from "@/style/modalStyle";
+import ModalDate from "./ModalDate";
+import ModalContentContainer from "./ModalContentContainer";
+
+interface ModalProps {
+  onClickShowModal: any;
+  showModal: boolean;
+  modalInfo: { status: string; data: object };
+}
+
+export default function Modal({
+  onClickShowModal,
+  showModal,
+  modalInfo,
+}: ModalProps) {
+  const [isToday, setIsToday] = useState(true);
+  const [pebbleStatusIdx, setPebbleStatusIdx] = useState(0);
+
+  function closeModal() {
+    onClickShowModal(!showModal);
+  }
+
+  function changePebbleStatus() {
+    if (pebbleStatusIdx === PEBBLE_STATUS.length - 1) {
+      setPebbleStatusIdx(0);
+    } else {
+      setPebbleStatusIdx(pebbleStatusIdx + 1);
+    }
+  }
+
+  function onClickSaveButton() {
+    console.log("큐티 저장!");
+  }
+
+  return (
+    <div className="background">
+      <div className="modal">
+        <div onClick={closeModal} className="close-modal">
+          <AiOutlineClose size="20" />
+        </div>
+
+        {modalInfo.status === "add" ? (
+          <>
+            <ModalDate isToday={isToday} setIsToday={setIsToday} />
+            <div onClick={changePebbleStatus}>
+              <PebbleImage pebbleStatus={PEBBLE_STATUS[pebbleStatusIdx]} />
+            </div>
+            <ModalContentContainer />
+            <div className="save-button">
+              <GreenButton
+                onClickButton={onClickSaveButton}
+                text="저장하기"
+                buttonId="saveNewQT"
+                size="large"
+              />
+            </div>
+          </>
+        ) : modalInfo.status === "view" ? (
+          <>
+            <ModalDate isToday={isToday} setIsToday={setIsToday} />
+            <div onClick={changePebbleStatus}>
+              <PebbleImage pebbleStatus={PEBBLE_STATUS[pebbleStatusIdx]} />
+            </div>
+            <ModalContentContainer />
+            <div className="save-button">
+              <GreenButton
+                onClickButton={onClickSaveButton}
+                text="저장하기"
+                buttonId="saveNewQT"
+                size="large"
+              />
+            </div>
+          </>
+        ) : (
+          <>
+            <ModalDate isToday={isToday} setIsToday={setIsToday} />
+            <div onClick={changePebbleStatus}>
+              <PebbleImage pebbleStatus={PEBBLE_STATUS[pebbleStatusIdx]} />
+            </div>
+            <ModalContentContainer />
+            <div className="save-button">
+              <GreenButton
+                onClickButton={onClickSaveButton}
+                text="저장하기"
+                buttonId="saveNewQT"
+                size="large"
+              />
+            </div>
+          </>
+        )}
+      </div>
+      <style jsx>{modalStyle}</style>
+    </div>
+  );
+}
---
+++ src/app/components/modal/ModalContentContainer.tsx
@@ -0,0 +1,85 @@
+import { QT_CONTENT } from "@/app/service/constants";
+
+export default function ModalContentContainer() {
+  return (
+    <div className="modal-content">
+      {QT_CONTENT.map((content, idx) => {
+        return (
+          <div key={idx} className="content-container">
+            <div>
+              <p>{content}</p>
+            </div>
+            {idx === 0 ? <input type="text"></input> : <textarea></textarea>}
+          </div>
+        );
+      })}
+      <style jsx>{`
+        .modal-content {
+          display: flex;
+          flex-direction: column;
+          justify-content: space-evenly;
+          width: 85%;
+          height: 80%;
+          margin: auto;
+          margin-top: 1rem;
+        }
+        .modal-content p {
+          display: flex;
+          padding-left: 0.5rem;
+          margin: 0;
+          align-items: center;
+        }
+        .modal-content .content-container {
+          margin: 0.2rem 0 0.2rem 0;
+        }
+        .modal-content div:first-child {
+          height: 40%;
+          display: flex;
+          flex-direction: row;
+        }
+        .modal-content div:first-child div {
+          width: 35%;
+          height: 100%;
+          border-radius: 0.3125rem 0rem 0rem 0.3125rem;
+          border-top: 0.5px solid #808080;
+          border-bottom: 0.5px solid #808080;
+          border-left: 0.5px solid #808080;
+          background: var(--afd-6-c-7, #afd6c7);
+        }
+        .modal-content div:first-child input {
+          width: 65%;
+          height: 100%;
+          border-radius: 0rem 0.3125rem 0.3125rem 0rem;
+          border: 0.5px solid var(--808080, #808080);
+          background: #fff;
+          padding: 0 0 0 0.3rem;
+          outline: none;
+        }
+        .modal-content div {
+          height: 100%;
+          display: flex;
+          flex-direction: column;
+        }
+        .modal-content div div {
+          width: 100%;
+          height: 70%;
+          border-radius: 0.3125rem 0.3125rem 0rem 0rem;
+          border-top: 0.5px solid #808080;
+          border-right: 0.5px solid #808080;
+          border-left: 0.5px solid #808080;
+          background: var(--afd-6-c-7, #afd6c7);
+        }
+        .modal-content textarea {
+          width: 96%;
+          height: 100%;
+          border-radius: 0rem 0rem 0.3125rem 0.3125rem;
+          border: 0.5px solid var(--808080, #808080);
+          background: #fff;
+          padding: 2%;
+          resize: none;
+          outline: none;
+        }
+      `}</style>
+    </div>
+  );
+}
